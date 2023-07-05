0f17c54f5ad5148aaf67f44c49b622183e39ff76
coding-learn11 <82395877+coding-learn11@users.noreply.github.com>
2023-07-05 23:06:31 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    background(100, 100, 100)
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('Filter_Image.png')
+}
