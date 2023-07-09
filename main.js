92633f7277a010aa03d8b64eb8aec7a965104ece
coding-learn11 <82395877+coding-learn11@users.noreply.github.com>
2023-07-09 18:49:15 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX = 0;
+noseY = 0;
 function preload() {
-
+   moustache = loadImage('https://i.postimg.cc/gcXZ12ww/mustache.png');
 }
 
 function setup() {
@@ -16,6 +18,7 @@ function setup() {
 
 function draw() {
 image(video, 0, 0, 300, 300);
+image(moustache, noseX, noseY, 65, 20);
 }
 
 function take_snapshot() {
@@ -30,7 +33,9 @@ function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
-        console.log('nose x =' + results[0].pose.nose.x);
-        console.log('nose y =' + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 22;
+        noseY = results[0].pose.nose.y + 10;
+        console.log('nose x = ' + noseX);
+        console.log('nose y = ' + noseY);
     }
 }
